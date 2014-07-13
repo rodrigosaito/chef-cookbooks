af001a8dfb92405446a8a53adedf08f5a18dc9b4
Rodrigo Saito <rodrigo.saito@gmail.com>
2014-07-13 10:07:38 -0300

    Java 7 cookbook

--- java7/recipes/default.rb
+++ java7/recipes/default.rb
@@ -6,3 +6,19 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+include_recipe "apt"
+
+apt_repository "ppa_webupd8team_java" do
+  uri "http://ppa.launchpad.net/webupd8team/java/ubuntu"
+  distribution node['lsb']['codename']
+  components ["main"]
+  keyserver "keyserver.ubuntu.com"
+  key "EEA14886"
+end
+
+execute "echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections" do
+end
+
+package "oracle-java7-installer" do
+  action :install
+end
